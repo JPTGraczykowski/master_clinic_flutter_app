deae27e4957fc00458d4c3a421511daf43389031
jedrzej <jedrzej.graczykowski@htdevelopers.com>
2023-08-26 01:26:37 +0200

    MCF-04 create a cabinets screen

---
+++ lib/screens/doctor/doctor_cabinets.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../../widgets/shared/screen_title.dart';
+import '../../models/cabinet.dart';
+import '../../widgets/cabinet_list_item.dart';
+
+class DoctorCabinetsScreen extends StatelessWidget {
+  const DoctorCabinetsScreen({
+    super.key,
+    required this.cabinets,
+  })
+
+  final List<Cabinet> cabinets;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: ScreenTitle(
+          title: 'My Cabinets',
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: cabinets.length,
+        prototypeItem: CabinetListItem(
+          cabinet: cabinets.first,
+        ),
+        itemBuilder: (context, index) {
+          return CabinetListItem(
+            cabinet: cabinets[index],
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/widgets/cabinet_list_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import '../../models/cabinet.dart';
+
+class CabinetListItem extends StatelessWidget {
+  const CabinetListItem({
+    super.key,
+    required this.cabinet,
+  });
+
+  final Cabinet cabinet;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.symmetric(
+        vertical: 7.5,
+        horizontal: 0,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 15,
+          horizontal: 0,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Cabinet: ${cabinet.name}',
+              style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            Text(
+              'Floor: ${cabinet.floor}',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
