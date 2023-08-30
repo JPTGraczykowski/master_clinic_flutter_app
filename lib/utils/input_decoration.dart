3bb7b3e6a512170061c7d6bf7327522c4a563512
jedrzej <jedrzej.graczykowski@htdevelopers.com>
2023-08-30 21:30:24 +0200

    MCF-06 Create an Input Decoration object in the form helper

---
+++ lib/utils/input_decoration.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class FormHelper {
+  static InputDecoration inputDecoration({
+    required BuildContext context,
+    InputBorder? enabledBorder,
+    InputBorder? border,
+    Color? fillColor,
+    bool? filled,
+    Widget? prefixIcon,
+    String? hintText,
+    String? labelText,
+  }) =>
+      InputDecoration(
+        enabledBorder: enabledBorder ??
+            OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.primary, width: 1.5)),
+        border: border ?? OutlineInputBorder(borderSide: BorderSide()),
+        fillColor: fillColor ?? Colors.white,
+        filled: filled ?? true,
+        prefixIcon: prefixIcon,
+        hintText: hintText,
+        labelText: labelText,
+      );
+}
