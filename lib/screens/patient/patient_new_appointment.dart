5eb7bec9e3944811c894a27f952a770118cf9e99
jedrzej <jedrzej.graczykowski@htdevelopers.com>
2023-08-30 18:36:37 +0200

    MCF-06 Add a screen for a new appointment for patients

---
+++ lib/screens/patient/patient_new_appointment.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PatientNewAppointmentScreen extends StatelessWidget {
+  const PatientNewAppointmentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
